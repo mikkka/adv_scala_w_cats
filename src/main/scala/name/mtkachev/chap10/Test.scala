90857b14fc82589b4009ae75f6052fe8778c7227
m.tkachev <m.tkachev@tinkoff.ru>
2017-10-04 16:00:33 +0500

    data validation: test code

--- src/main/scala/name/mtkachev/chap10/Test.scala
+++ src/main/scala/name/mtkachev/chap10/Test.scala
@@ -1,21 +1,66 @@
 package name.mtkachev.chap10
 
+import cats.data.Validated
+import cats.data.Validated._
+
 import cats.instances.list._
 
+import cats.syntax.apply._
+
 object Test extends App {
-  val a: Predicate[List[String], Int] =
-    Pure { v =>
-      if(v > -2) None
-      else Some(List("Must be > -2"))
+  type Errors = List[String]
+  type ValOr[T] = Validated[Errors, T]
+
+  def error(s: String): List[String] =
+    List(s)
+
+  def longerThan(n: Int): Predicate[Errors, String] =
+    Predicate.lift(
+      error(s"Must be longer than $n characters"),
+      str => str.size > n)
+
+  val alphanumeric: Predicate[Errors, String] =
+    Predicate.lift(
+      error(s"Must be all alphanumeric characters"),
+      str => str.forall(_.isLetterOrDigit))
+
+  def contains(char: Char): Predicate[Errors, String] =
+    Predicate.lift(
+      error(s"Must contain the character $char"),
+      str => str.contains(char))
+
+  def containsOnce(char: Char): Predicate[Errors, String] =
+    Predicate.lift(
+      error(s"Must contain the character $char only once"),
+      str => str.filter(c => c == char).size == 1)
+
+  val alphanumeric4 =
+    Check(alphanumeric and longerThan(3))
+
+  val emailSplitCheck: Check[Errors, String, (String, String)] = Check(
+    x => x.split('@') match {
+      case Array(lft, rgt) =>
+        Valid((lft, rgt))
+      case other =>
+        Invalid(error("must contain @"))
     }
+  )
 
-  val b: Predicate[List[String], Int] =
-    Pure { v =>
-      if(v < 2) None
-      else Some(List("Must be < 2"))
-    }
+  val checkEmailLeft: Check[Errors, String, String] = Check(longerThan(0))
+  val checkEmailRgt : Check[Errors, String, String] = Check(longerThan(3) and contains('.'))
 
-  val check = a and b
+  val joinEmailCheck: Check[Errors, (String, String), String] = Check {case (l, r) =>
+    (checkEmailLeft(l): ValOr[String],
+      checkEmailRgt(r): ValOr[String]).mapN((ll, rr) => ll + "@" + rr)
+  }
 
-  println(check(0))
+  val checkEmail: Check[Errors, String, String] = emailSplitCheck andThen joinEmailCheck
+
+  final case class User(username: String, email: String)
+
+  def createUser(username: String, email: String): Validated[Errors, User] =
+    (alphanumeric4(username): ValOr[String], checkEmail(email): ValOr[String]).mapN(User)
+
+  println(createUser("Noel", "noel@underscore.io"))
+  println(createUser("", "dave@underscore@io"))
 }
